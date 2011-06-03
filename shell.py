42e6eed4b258e5b9d666eb4ee4c4620a9cd417ad
Dylan Nugent <dylnuge@gmail.com>
2011-06-02 20:57:41 -0500

    [shell] Implemented command processing, need to make less hardcoded

--- shell.py
+++ shell.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-# Initial workings of the shell interface
+# A simple shell interface for Adventure Core
 # This software is licensed under the NCSA License. Details in LICENSE.txt
 # Copyright 2011 Dylan Nugent. All rights reserved.
 
@@ -24,6 +24,7 @@ class ShellInterface:
 	initmessage = textwrap.dedent(initmessage)
 	prompt = "command> "
 	case_insensitive = True
+	command_list = {}
 
 	def __init__(self):
 		"""Initializes the shell interface.
@@ -35,6 +36,7 @@ class ShellInterface:
 		if DEBUG:
 			print("Debug mode is active. Bumpy road ahead.")
 		
+		self.load()
 		self.running = False
 
 	def run(self):
@@ -42,28 +44,58 @@ class ShellInterface:
 
 		Note that this will take over an application until run terminates.
 		"""
+
 		self.running = True
 
 		while(self.running):
-			command = self.get_command()
-			self.process_command(command)
+			indata = self.get_command()
+			try:
+				self.process_command(indata)
+			except NotImplementedError:
+				print("Error: Command not yet implemented")
+			except KeyError as e:
+				print("Error: {}".format(e))
+
+	def load(self):
+		"""Load up the commands for the shell interface."""
+
+		# TODO: Come up with a better way to do this than hardcoded commands
+		self.command_list["help"] = "Display this help message"
+		self.command_list["quit"] = "Exit the shell"
+		self.command_list["load"] = "Load an Adventure Core game into the shell"
 
 	def get_command(self):
 		"""Display a shell prompt and get the command from it"""
+
 		command = input(self.prompt)
 		if self.case_insensitive:
 			command = command.lower()
 
 		return command
 
-	def process_command(self, command):
+	def process_command(self, linein):
 		"""Process a command on the active shell
 		
 		Arguments:
-		command - The command to be processed
+		linein - The command to be processed
 		"""
-		if(command == "quit"):
-			self.running = False
+
+		# For now, it's a hard coded limitation that a command must be
+		# distinguished by its first word alone, and all following data
+		# is to be treated as an argument to the command
+		command = linein.split()[0]
+		args = linein.split()[1:]
+
+		if(command in self.command_list):
+			# Process the command, currently manual
+			if(command == "help"):
+				print(self.command_list)
+			elif(command == "quit"):
+				self.running = False
+			else:
+				raise NotImplementedError()
+		else:
+			raise KeyError("No such command")
 
 
 # For current testing purposes, allow the shell to be run directly
